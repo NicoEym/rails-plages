272c12c1bd352e9382b53db62c181ea21a3dbe14
nicolas <nicolas.eymenier@gmail.com>
2020-06-06 19:07:14 -0300

    controllers and seed created

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :beach
   belongs_to :calendar
-  has_many :users, rank_name: 'member'
-  has_one :user, rank_name: 'chief'
+  # belongs_to :users, rank_name: 'member'
+  # has_one :user, rank_name: 'chief'
 end
