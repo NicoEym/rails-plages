659fb0aa7909ed50e6c71fa3d6dab627811feb19
nicolas <nicolas.eymenier@gmail.com>
2020-06-08 23:15:57 -0300

    started the calendar controller and views

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,2 +1,14 @@
 class CalendarsController < ApplicationController
+
+
+  def index
+    @dates = Calendar.all
+  end
+
+  def show
+    @date = Calendar.find(params[:id])
+    @beaches = Beach.all
+    @teams = Team.find_by(calendar: @date)
+  end
+
 end
