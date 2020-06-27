c53f6c52e08b9b11bd8797e2dd0458aa8a7661bb
nicolas <nicolas.eymenier@gmail.com>
2020-06-27 00:13:02 -0300

    ok JS to create a team, modify a team and force to choose all the users

--- app/controllers/team_lifeguards_controller.rb
+++ app/controllers/team_lifeguards_controller.rb
@@ -4,15 +4,15 @@ class TeamLifeguardsController < ApplicationController
     @beach = Beach.find(params[:beach_id])
     @team = Team.find(params[:team_id])
     @team_lifeguard = TeamLifeguard.new
-    @leader = Rank.find_by(name: "Chef de poste")
-    @team_mate = Rank.find_by(name: "Equipier")
+    @leader_rank = Rank.find_by(name: "Chef de poste")
+    @team_mate_rank = Rank.find_by(name: "Equipier")
     @team_mate_number = @beach.number_of_team_members - 1
   end
 
   def create
     @team_lifeguard = TeamLifeguard.new(team_lifeguard_params)
     @team_lifeguard.team = Team.find(params[:team_id])
-    @date = @team_lifeguard.team.calendar
+
     @team_lifeguard.save
     if @team_lifeguard.save
       # redirect_to calendar_path(@date.id)
@@ -22,13 +22,19 @@ class TeamLifeguardsController < ApplicationController
   end
 
   def edit
-    # @date = Calendar.find(params[:calendar_id])
-    # @beach = Beach.find(params[:beach_id])
-    # @team = Team.find(params[:team_id])
-    # @leader = Rank.find_by(name: "Chef de poste")
-    # @team_mate = Rank.find_by(name: "Equipier")
-    # @team_mate_number = @beach.number_of_team_members - 1
-    # team_lifeguards = TeamLifeguard.new
+    @team = Team.find(params[:team_id])
+    @team_lifeguards = TeamLifeguard.where(team: @team)
+    @beach = @team.beach
+    @date = @team.calendar
+    @leader_rank = Rank.find_by(name: "Chef de poste")
+    @team_mate_rank = Rank.find_by(name: "Equipier")
+    @leader = @team_lifeguards.first
+    @team_mates = @team_lifeguards.drop(1)
+  end
+
+  def update
+    @team_lifeguard = TeamLifeguard.find(params[:id])
+    @team_lifeguard.update(team_lifeguard_params)
   end
 
   private
