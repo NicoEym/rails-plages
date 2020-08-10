b933c2d1310860f0985edb07c0db2f89a552fa86
nicolas <nicolas.eymenier@gmail.com>
2020-08-09 21:24:31 -0300

    inserted more params to the devise form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile, :rank_id, :avatar_url])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :mobile, :avatar_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :mobile, :photo])
   end
 
   include Pundit
@@ -26,7 +25,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     if current_user.admin?
-       Season.count == 0 ? "/seasons/new" : "/seasons/"# <- Path you want to redirect the user to.
+      Season.count == 0 ? "/seasons/new" : "/seasons/"# <- Path you want to redirect the user to.
     else
       "/teams/"
     end
